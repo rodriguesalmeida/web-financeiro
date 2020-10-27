c8bd2f6323afe14b8a8a08fa612833786cb8b061
Anderson Almeida <anderson.almeida@devionn.com>
2020-10-26 22:23:40 -0200

    correcoes do rall

--- src/app/pages/usuario/usuario-pesquisa/usuario-pesquisa.component.ts
+++ src/app/pages/usuario/usuario-pesquisa/usuario-pesquisa.component.ts
@@ -2,39 +2,50 @@ import { Usuario } from './../../../models/usuario';
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-usuario-pesquisa',
   templateUrl: './usuario-pesquisa.component.html',
-  styleUrls: ['./usuario-pesquisa.component.scss']
+  styleUrls: ['./usuario-pesquisa.component.scss'],
+  providers:[
+    UsuarioService
+  ]
 })
 export class UsuarioPesquisaComponent implements OnInit {
   public usuarios:Usuario[] = [];
   public dataSource:MatTableDataSource<Usuario> = new MatTableDataSource(this.usuarios);
-  public displayedColumns:string[] = ['nome','login','senha','acoes'] ;
-  
-  constructor(private router:Router) { }
+  public displayedColumns:string[] = ['id','nome','login','senha','acoes'] ;
+  public form:FormGroup = new FormGroup({
+    pesquisa:new FormControl('')
+  });
+  constructor(private router:Router, private usuarioService:UsuarioService) { }
 
   ngOnInit(): void {
-    let jsonUsers = localStorage.getItem('usuarios');
-    if (jsonUsers != null){
-      this.usuarios = JSON.parse(jsonUsers);
-    }
-    this.dataSource = new MatTableDataSource(this.usuarios);
-    
+    this.pesquisar();    
   }
-
+  public pesquisar(){
+    let nome = this.form.controls['pesquisa'].value;
+    this.usuarioService.pesquisar(nome).subscribe((lista)=>{
+      this.usuarios = lista;
+      this.dataSource = new MatTableDataSource(this.usuarios);
+    });
+  }
   public remover(usuario){
-    console.log('Removendo o usuario',usuario)
-    let index = this.usuarios.indexOf(usuario);
-    this.usuarios.splice(index, 1);
-    localStorage.setItem('usuarios',JSON.stringify(this.usuarios));
-    this.dataSource = new MatTableDataSource(this.usuarios);
+    this.usuarioService.excluir(usuario.id).subscribe((res)=>{
+      console.log('Removendo o usuario',usuario)
+      let index = this.usuarios.indexOf(usuario);
+      this.usuarios.splice(index, 1);
+      this.dataSource = new MatTableDataSource(this.usuarios);
+    });    
   }
-
   public alterar(usuario){
     console.log('Alterando o usuario',usuario)
     this.router.navigate(['/usuario/cadastro', usuario]); 
+  }
+  public novo(){
+    this.router.navigate(['/usuario/cadastro']); 
   }
 
 }
